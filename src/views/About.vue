041545f42c8551ab10a42a55e7b0c8864bbff285
Ethea2 <nathandres2001@gmail.com>
2021-11-29 22:06:46 +0400

    solved navbar issues by adding a section to about.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,21 @@
 <template>
-<div class="container">
-  <div class="box-1">
-    <h1>Hallo!</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea corrupti dolores porro corporis laboriosam, rerum amet ratione, quod dolorum officiis doloribus, eius cupiditate modi repellat? Maxime officiis provident minima quis!</p>
+<section class="about-section">
+    <div class="container-1">
+    <div class="box-1">
+      <h1>Hallo!</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea corrupti dolores porro corporis laboriosam, rerum amet ratione, quod dolorum officiis doloribus, eius cupiditate modi repellat? Maxime officiis provident minima quis!</p>
+    </div>
+    <div class="box-2">
+      <h1>Hallo!</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea corrupti dolores porro corporis laboriosam, rerum amet ratione, quod dolorum officiis doloribus, eius cupiditate modi repellat? Maxime officiis provident minima quis!</p>
+    </div>
+    <div class="box-3">
+      <h1>Hallo!</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea corrupti dolores porro corporis laboriosam, rerum amet ratione, quod dolorum officiis doloribus, eius cupiditate modi repellat? Maxime officiis provident minima quis!</p>
+    </div>
   </div>
-  <div class="box-2">
-    <h1>Hallo!</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea corrupti dolores porro corporis laboriosam, rerum amet ratione, quod dolorum officiis doloribus, eius cupiditate modi repellat? Maxime officiis provident minima quis!</p>
-  </div>
-  <div class="box-3">
-    <h1>Hallo!</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea corrupti dolores porro corporis laboriosam, rerum amet ratione, quod dolorum officiis doloribus, eius cupiditate modi repellat? Maxime officiis provident minima quis!</p>
-  </div>
-</div>
+</section>
+
 
   
 </template>
@@ -24,7 +27,7 @@ export default {
 </script>
 
 <style>
-div{
+.about-section div{
   border: 1px solid black;
 }
 .container-1{
